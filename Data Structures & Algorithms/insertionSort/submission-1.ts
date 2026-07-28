1d27d0a01e3d6509612730b66f1e2dc072029f02
amolas97 <67504232+amolas97@users.noreply.github.com>
2026-07-28 15:48:53 -0300

    Add: insertionSort - submission-1

---
+++ Data Structures & Algorithms/insertionSort/submission-1.ts
@@ -0,0 +1,35 @@
+/**
+ * Pair class to store key-value pairs
+ */
+// class Pair {
+//     /**
+//      * @param {number} key The key to be stored in the pair
+//      * @param {string} value The value to be stored in the pair
+//      */
+//     constructor(key, value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[][]}
+     */
+    insertionSort(pairs: Pair[]): Pair[][] {
+        const response: Pair[][] = []
+        const paircp: Pair[] = [...pairs];
+        if(paircp.length > 0) response.push([...paircp])
+        for(let i = 1; i < paircp.length; i++){
+            let j = i - 1;
+            while(j >= 0 && paircp[j+1].key < paircp[j].key){
+                const temp = paircp[j+1];
+                paircp[j+1] = paircp[j];
+                paircp[j] = temp;
+                j--;
+            }
+            response.push([...paircp])
+        }
+        return response;
+    }
+}
