47bffc78a3a5dc0823e550f68cb2102b9e666e3d
amolas97 <67504232+amolas97@users.noreply.github.com>
2026-08-01 06:27:32 -0300

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        
+        let left = 0
+        let right = nums.length - 1
+
+        while(right >= left){
+
+          let mid = Math.floor((right - left) / 2) + left
+          if(nums[mid] === target) return mid
+          if(nums[mid] < target){
+            left = mid + 1
+          } else {
+            right = mid - 1
+          }
+  
+
+        }
+        return -1
+    }
+}
