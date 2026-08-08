1c2795bb658315037cadda636843bb01f900a5b5
amolas97 <67504232+amolas97@users.noreply.github.com>
2026-08-08 05:09:20 -0300

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums: number[]): number[][] {
+        const result = [];
+        function backtrack(start, path) {
+          result.push([...path]);
+          for (let i = start; i < nums.length; i++) {
+            path.push(nums[i]);
+            backtrack(i + 1, path);
+            path.pop();
+          }
+        }
+        backtrack(0, []);
+        return result;
+    }
+}
